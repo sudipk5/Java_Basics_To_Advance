483fd39cac4fffd181d2b6fb8a5e024b38611d38
skundu <sudipkundu861@gmail.com>
2025-09-01 11:49:22 +0530

    Least Common Multiple

---
+++ src/LMC.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class LMC {
+        public static void main(String[] args){
+            Scanner input=new Scanner(System.in);
+
+            int num1;
+            System.out.println("Enter 1st number = ");
+            num1=input.nextInt();
+            int num2;
+            System.out.println("Enter 1st number = ");
+            num2=input.nextInt();
+            System.out.println(Lmc(num1,num2));
+
+        }
+        public  static  int Lmc(int number1 ,int number2){
+            int i=1;
+            while (true){
+                int factor=number1*i;
+                if(factor%number2==0){
+                   return factor;
+
+                }
+                i++;
+            }
+        }
+}
--- src/Sum_Of_Digits.java
+++ src/Sum_Of_Digits.java
@@ -1,29 +1,18 @@
 import java.util.Scanner;
-
 public class Sum_Of_Digits {
-
     public static void main(String[]args){
         Scanner input= new Scanner(System.in);
         int num;
         System.out.println("Enter any number");
         num=input.nextInt();
         System.out.println(Digits(num));
-
-
     }
-
     public static int Digits(int num){
-
         int i=0;
-
         while (num>0){
-
             i+= (num%10);
             num=num/10;
-
         }
        return i;
-
-
     }
 }
