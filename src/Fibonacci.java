a899d8a72b8ca23fc259ed23d11fb95a69c21773
skundu <sudipkundu861@gmail.com>
2025-09-04 22:46:43 +0530

    prime and updatd file

---
+++ src/Fibonacci.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+
+public static void main(String[]args){
+    Scanner input=new Scanner(System.in);
+    int a;
+    System.out.println("Enter a valu");
+    a=input.nextInt();
+
+
+    int f=0;
+    int s=1;
+
+
+    while (f+s<=a){
+
+
+
+        int t=f+s;
+
+
+        System.out.println(t +"");
+
+        f=s;
+        s=t;
+
+
+
+
+
+    }
+
+}
+
+}
