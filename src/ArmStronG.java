b6cf0180ce8aa2ec3cd6e0e3e3e790bb21e81411
skundu <sudipkundu861@gmail.com>
2025-09-04 23:31:44 +0530

    Armstrong code

---
+++ src/ArmStronG.java
@@ -0,0 +1,22 @@
+public class ArmStronG {
+    public static void main(String[]args){
+        int a=153;
+        int b=0;
+        int c=0;
+        int d=0;
+        int i=0;
+        while ( i <=a){
+            b=a%10;
+
+            c=b*b*b;
+            d=d+c;
+
+            System.out.println(d);
+            i++;
+
+
+
+        }
+
+    }
+}
--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -7,28 +7,19 @@ public static void main(String[]args){
     int a;
     System.out.println("Enter a valu");
     a=input.nextInt();
-
-
     int f=0;
     int s=1;
 
 
     while (f+s<=a){
 
-
-
         int t=f+s;
-
 
         System.out.println(t +"");
 
+
         f=s;
         s=t;
-
-
-
-
-
     }
 
 }
